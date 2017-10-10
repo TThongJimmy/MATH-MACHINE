7c6c61c56bf9b1dd2d2259fde5d9b49a6a4fbf87
Jimmy <tutuhong@protonmail.com>
2017-10-10 20:17:13 +0800

    整理

--- main.c
+++ main.c
@@ -3,17 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    double x, y;
-    while(1)
-    {
-        printf("请输入需要四舍五入的数字\n");
-        scanf("%lf",&x);
+    MC_change(123456789);
 
-        printf("请输入需要四舍五入到几位\n");
-        scanf("%lf",&y);
-        
-
-        printf("%lf\n\n",D_round(x,y));
-    }
+	getch();
     return 0;
 }
